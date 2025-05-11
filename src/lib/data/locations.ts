fa297741c4f0b0616e1f2fc2d77fcfc45946b69c
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-05-10 22:03:28 -0700

    feat: some extra seo changes

--- src/lib/data/locations.ts
+++ src/lib/data/locations.ts
@@ -3,7 +3,7 @@ import { PUBLIC_DOMAIN } from "$env/static/public";
 export const contactInfo = {
   phone: '7788045911',
   email: `info@${PUBLIC_DOMAIN}`,
-  address: '1234 Main St, Vancouver, BC V5K 0A1',
+  address: '12763 62 Ave, Surrey, Canada, V3X3M5',
   instagram: 'https://www.instagram.com/rightmarketing.ca/',
   facebook: 'https://www.facebook.com/profile.php?id=61573842316284',
   tiktok: ''
