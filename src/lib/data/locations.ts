8f52c176998debdfd22565f5e387502fe71bca2b
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-05-13 11:21:38 -0700

    feat: updated phone number

--- src/lib/data/locations.ts
+++ src/lib/data/locations.ts
@@ -1,7 +1,7 @@
 import { PUBLIC_DOMAIN } from "$env/static/public";
 
 export const contactInfo = {
-  phone: '7788045911',
+  phone: '7784865911',
   email: `info@${PUBLIC_DOMAIN}`,
   address: '12763 62 Ave, Surrey, Canada, V3X3M5',
   instagram: 'https://www.instagram.com/rightmarketing.ca/',
